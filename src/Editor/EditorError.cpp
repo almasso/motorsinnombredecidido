68fe03d731ed5a0626dda25bc1e901707b1afffe
almasso <almasso@ucm.es>
2025-02-18 13:45:05 +0100

    Inicio del editor

---
+++ src/Editor/EditorError.cpp
@@ -0,0 +1,6 @@
+//
+// MIT License
+// Copyright (c) 2025 Alejandro Massó Martínez, Miguel Curros García, Alejandro González Sánchez
+//
+
+#include "EditorError.h"
---
+++ src/Editor/EditorError.h
@@ -0,0 +1,21 @@
+//
+// MIT License
+// Copyright (c) 2025 Alejandro Massó Martínez, Miguel Curros García, Alejandro González Sánchez
+//
+
+#ifndef MOTORSINNOMBREDECIDIDO_EDITORERROR_H
+#define MOTORSINNOMBREDECIDIDO_EDITORERROR_H
+
+/**
+ * @~english
+ * @brief Class that will contain all Editor possible errors, and methods to show them to the programmer or user
+ *
+ * @~spanish
+ * @brief Clase que contendrá todos los posibles errores del Editor, así como métodos para poder mostrarlos al programador o usuario
+ */
+class EditorError {
+
+};
+
+
+#endif //MOTORSINNOMBREDECIDIDO_EDITORERROR_H
